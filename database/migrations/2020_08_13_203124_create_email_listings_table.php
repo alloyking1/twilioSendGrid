72888d36d3bdcd261a134a3395833f66fa18061f
alloyking1 <alloyking1@gmail.com>
2020-08-13 16:42:49 -0400

    modified the create_email_listing migration table to adding the email field

---
+++ database/migrations/2020_08_13_203124_create_email_listings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_listings', function (Blueprint $table) {
+            $table->id();
+            $table->email();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_listings');
+    }
+}
