f32a5a50987f057668338f667254562042ed0671
alloyking1 <alloyking1@gmail.com>
2021-03-12 12:10:47 -0500

    made lots of changes to the email sending method. Email sending now works

--- app/Http/Controllers/EmailListingController.php
+++ app/Http/Controllers/EmailListingController.php
@@ -10,43 +10,103 @@ class EmailListingController extends Controller
 {
     public function sendEMail(Request $request)
     {
+
         $validated = $request->validate([
             'emailTopic' => 'required|string',
             'emailBody' => 'required|string',
             'senderEmail' => 'required|email',
         ]);
 
+        $from = $validated['senderEmail'];
+        $topic = $validated['emailTopic'];
+        $addresses = EmailListing::all();
+        $receivers = [];
+        $emailContent = $validated['emailBody'];
+
+        foreach($addresses as $address){
+            $receivers2 = [$address->email => "this is a test"];
+            // $receivers[] = $receivers2;
+        }
+
+        // return $receivers;
+
+        // $tos = [
+        //         "alloyking1@gmail.com" => "Example User1",
+        //         "alloyking1@yahoo.com" => "Example User2",
+        //     ];
+            
+
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom($from, "alloy");
+        $email->setSubject($topic);
+        // $email->addTo("alloyking1@gmail.com", "Example User");
+        $email->addTos($receivers2);
+        $email->addContent("text/plain", $emailContent);
+        
+        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+
+        try {
+            $response = $sendgrid->send($email);
+            return response()->json("Email sent successfully");
+
+        } catch (Exception $e) {
+            return response()->json( 'Caught exception: '. $e->getMessage() ."\n");
+        }
+
+
+
+
+
+
+
+
         $from = new \SendGrid\Mail\From($validated['senderEmail']);
 
         /* Add email array from db to the mailing list */
         $receivers = EmailListing::all();
+        foreach ($receivers as $key){
 
-        /* Sent subject of mail */
-        $subject = new \SendGrid\Mail\Subject($validated['emailTopic']);
+            /* Sent subject of mail */
+            $subject = new \SendGrid\Mail\Subject($validated['emailTopic']);
 
-        /* Set mail body */
-        $htmlContent = new \SendGrid\Mail\HtmlContent($validated['emailBody']);
+            /* Set mail body */
+            $htmlContent = new \SendGrid\Mail\HtmlContent($validated['emailBody']);
 
-        $email = new \SendGrid\Mail\Mail(
-            $from,
-            $receivers,
-            $subject,
-            null,
-            $htmlContent
-        );
+            $email = new \SendGrid\Mail\Mail(
+                $from,
+                $key->email,
+                $subject,
+                null,
+                $htmlContent
+            );
 
-        return response()->json($mail);
-
-        /* Create instance of Sendgrid SDK */
-        $sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
-
-        /* Send mail using sendgrid instance */
-        $response = $sendgrid->send($email);
-        if ($response->statusCode() == 202) {
-            return back()->with(['success' => "E-mails successfully sent out!!"]);
+            return $response = $sendgrid->send($email);
         }
 
-        return back()->withErrors(json_decode($response->body())->errors);
+        // /* Sent subject of mail */
+        // $subject = new \SendGrid\Mail\Subject($validated['emailTopic']);
+
+        // /* Set mail body */
+        // $htmlContent = new \SendGrid\Mail\HtmlContent($validated['emailBody']);
+
+        // $email = new \SendGrid\Mail\Mail(
+        //     $from,
+        //     $receivers,
+        //     $subject,
+        //     null,
+        //     $htmlContent
+        // );
+
+        // /* Create instance of Sendgrid SDK */
+        // $sendgrid = new SendGrid(getenv('SENDGRID_API_KEY'));
+
+        /* Send mail using sendgrid instance */
+        // return $response = $sendgrid->send($email);
+        // if ($response->statusCode() == 202) {
+        //     return back()->with(['success' => "E-mails successfully sent out!!"]);
+        // }
+
+        // return back()->withErrors(json_decode($response->body())->errors);
     }
 
 }
